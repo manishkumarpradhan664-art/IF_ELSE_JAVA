afa7c7d8ef77975b6fea0c0f00fe828047daf89e
manishkumarpradhan664-art <manishkumarpradhan664@gmail.com>
2026-03-06 10:09:41 +0530

    Manish pradhan

---
+++ BMICheck.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+public class BMICheck {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter BMI value: ");
+double bmi = sc.nextDouble();
+if(bmi < 18.5)
+System.out.println("Underweight");
+else if(bmi < 25)
+System.out.println("Normal");
+else
+System.out.println("Overweight");
+}
+}
+
+
+
+
+
+1⃣7⃣ ⚖ BMI Category
+import java.util.Scanner;
+public class BMICheck {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter BMI value: ");
+double bmi = sc.nextDouble();
+if(bmi < 18.5)
+System.out.println("Underweight");
+else if(bmi < 25)
+System.out.println("Normal");
+Soumik Karmakar - Object Oriented Programming with Java
+else
+System.out.println("Overweight");
+}
+}
+
+
+1⃣7⃣ ⚖ BMI Category
+import java.util.Scanner;
+public class BMICheck {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter BMI value: ");
+double bmi = sc.nextDouble();
+if(bmi < 18.5)
+System.out.println("Underweight");
+else if(bmi < 25)
+System.out.println("Normal");
+Soumik Karmakar - Object Oriented Programming with Java
+else
+System.out.println("Overweight");
+}
+}
---
+++ Bonus.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Bonus {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter salary: ");
+double salary = sc.nextDouble();
+System.out.print("Enter years of service: ");
+int years = sc.nextInt();
+if(years > 5)
+else
+System.out.println("Total Salary with Bonus: " + (salary + salary * 0.1));
+System.out.println("No Bonus");
+}
+}
+
+
+
+
+
+import java.util.Scanner;
+public class Bonus {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter salary: ");
+double salary = sc.nextDouble();
+System.out.print("Enter years of service: ");
+int years = sc.nextInt();
+if(years > 5)
+else
+System.out.println("Total Salary with Bonus: " + (salary + salary * 0.1));
+System.out.println("No Bonus");
+}
+}
+
+
+
+
+
+
+import java.util.Scanner;
+public class Bonus {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter salary: ");
+double salary = sc.nextDouble();
+System.out.print("Enter years of service: ");
+int years = sc.nextInt();
+if(years > 5)
+else
+System.out.println("Total Salary with Bonus: " + (salary + salary * 0.1));
+System.out.println("No Bonus");
+}
+}
